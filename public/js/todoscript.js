1a724443dacbccbde7ac68dfeeeb777bb2a812be
Tanmay <tanmay.jain260@gmail.com>
2021-05-06 20:52:12 +0530

    added-delete-functionality

--- public/js/todoscript.js
+++ public/js/todoscript.js
@@ -4,7 +4,8 @@ $(
         let btnAdd = $('#btnAdd')
         let ulList = $('#ulList')
         let btnDelete = $('#btnDelete')
-        let listItems = $('li')
+        
+        
 
         
 
@@ -15,41 +16,48 @@ $(
                 console.log(newTodo)
 
                 $.post('/todos',{task:newTodo},(data)=>{
-                    for(let tsk of data){
-                        ulList.append('<li>'+ tsk.task+'</li>')
+                    for(let i=0;i<data.length;i++){
+                        
+                        ulList.append( $('<li></li>').attr('id', 'q' + i).text(data[i].task))
                     }
                 })
+                
 
             })
             
         btnDelete.click(function(){
              
-            //$('<button/>').attr({id:'del',text:'❌'})
-            //<button id="del">❌</button>
-            $.get('/todos',(data)=>{
-                let arrayLength = data.length
-                console.log(arrayLength)
-                let delBtn = $("<button></button>");
-                delBtn.innerText('X')
-                $('li').append(delBtn)
+                    $("li").each(function(index) {
+                    console.log(index + ": " + $(this).text());
+                    let delBtn = $("<input/>").attr({class:"xBtn",type:"button",value:"❌",}).attr('id','d'+index)
+                    $(this).append(delBtn)
+                    
+                        delBtn.click(function(){
+                            let delVal = $(this).parent().text()
+                            console.log(delVal)
+                            $(this).parent().remove();
+                            $.post('/todos/delete',{eleDel:delVal},(data)=>{
+                            console.log('deleting my self')
+                            
+                        })
+                    }) 
+                    
 
-                /*for(let i=1; i<=arrayLength; i++){
-                    let delBtn = $("<button></button>").attr({text:"❌"}); 
-                    console.log(delBtn)
-                    console.log(typeof delBtn)
-                    listItems.append(delBtn)
-                    console.log('appending the button')
-                }*/
-            })
-            //console.log('function started')
-            //console.log(listItems.length)
 
-           /* for(let i=1; i<=$('li').length; i++){
-                let delBtn = $("<button></button>").text("❌"); 
-                listItems.append(delBtn)
-                console.log('appending the button')
-            }*/
+
+                });
+                
+            
+
+            
+
         })
+
         
-    }
-)
+
+
+            
+            
+})
+        
+    
--- routes/todos.js
+++ routes/todos.js
@@ -11,4 +11,20 @@ route.post('/',(req,res,next)=>{
     )
     res.send(todoArray)
 })
+
+route.post('/delete',(req,res,next)=>{
+    console.log(req.body.eleDel)
+    let removeItem = req.body.eleDel ;
+
+    for(let i=0 ; i<todoArray.length ; i++){
+        if(todoArray[i].task == removeItem){
+            todoArray.splice(i, 1)
+        }
+    }
+    /*todoArray.push(
+        {task:req.body.task}
+    )*/
+    res.send(todoArray)
+})
+
 module.exports = route
