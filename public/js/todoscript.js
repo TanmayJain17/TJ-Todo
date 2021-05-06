b53ec1b0dd673539b252592fd1c036068511adee
Tanmay <tanmay.jain260@gmail.com>
2021-05-07 01:37:35 +0530

    done-deletion-functionality

--- public/js/todoscript.js
+++ public/js/todoscript.js
@@ -1,9 +1,11 @@
-$(
-    function() {
+$(document).ready(
+    function(){
         let inptTask = $('#inptTask')
         let btnAdd = $('#btnAdd')
         let ulList = $('#ulList')
         let btnDelete = $('#btnDelete')
+
+        
         
         
 
@@ -24,40 +26,50 @@ $(
                 
 
             })
+            let flag=1
             
         btnDelete.click(function(){
+            if(flag==1){
+                
+                flag=0
+            
+                let doneDel = $('<input/>').attr({id:'btnDoneDel',type:'button'}).attr('value','✔️')
+                btnDelete.after(doneDel)
+
              
-                    $("li").each(function(index) {
+                $("li").each(function(index) {
                     console.log(index + ": " + $(this).text());
                     let delBtn = $("<input/>").attr({class:"xBtn",type:"button",value:"❌",}).attr('id','d'+index)
                     $(this).append(delBtn)
                     
-                        delBtn.click(function(){
-                            let delVal = $(this).parent().text()
-                            console.log(delVal)
-                            $(this).parent().remove();
-                            $.post('/todos/delete',{eleDel:delVal},(data)=>{
-                            console.log('deleting my self')
-                            
-                        })
-                    }) 
+                    delBtn.click(function(){
+                        let delVal = $(this).parent().text()
+                        console.log(delVal)
+                        $(this).parent().remove();
+                        $.post('/todos/delete',{eleDel:delVal},(data)=>{
+                        console.log('deleting my self')
+                        
+                    })
                     
-
-
-
+                }) 
+                
                 });
+                let x=0;
+
+                $('#btnDoneDel').click(function(){
+                    flag=1;
+                    $('.xBtn').remove()
+                    $('#btnDoneDel').remove()
+                    x=1;
+
+                })
+                if(x==1){
+                    exit()
+                }
                 
             
 
             
-
+            }
         })
-
-        
-
-
-            
-            
 })
-        
-    
